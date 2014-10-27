7e43daac0597278e3c582fcd0b58324b2346a4f9
Ryan <rd02220@georgiasouthern.edu>
2014-10-27 13:39:28 -0400

    Basics

--- src/com/slotmachine/main/Main.java
+++ src/com/slotmachine/main/Main.java
@@ -29,25 +29,12 @@ public class Main {
 	// Button Event Variables
 	Boolean isDown = false, isDown2 = false;
 
-	// Font Variables
-	// public TrueTypeFont font;
-
-	// Display Variables
-	boolean vsync = false;
-
 	// Memory Variables
 	private static final long MEGABYTE = 1024L * 1024L;
 
 	// Timing and FPS Variables
 	long lastFrame, lastFPS;
 	int fps;
-
-	// World Variables
-	int worldID = 0;
-	public static boolean editMap = false;
-	public static boolean placeObjects = false;
-
-	// Player Variables
 
 	public Main() throws Exception {
 		try {
@@ -92,69 +79,6 @@ public class Main {
 		}
 
 		return image;
-	}
-
-	public void setDisplayMode(int width, int height, boolean fullscreen) {
-		// return if requested DisplayMode is already set
-		if ((Display.getDisplayMode().getWidth() == width)
-				&& (Display.getDisplayMode().getHeight() == height)
-				&& (Display.isFullscreen() == fullscreen)) {
-			return;
-		}
-
-		try {
-			DisplayMode targetDisplayMode = null;
-
-			if (fullscreen) {
-				DisplayMode[] modes = Display.getAvailableDisplayModes();
-				int freq = 0;
-
-				for (int i = 0; i < modes.length; i++) {
-					DisplayMode current = modes[i];
-
-					if ((current.getWidth() == width)
-							&& (current.getHeight() == height)) {
-						if ((targetDisplayMode == null)
-								|| (current.getFrequency() >= freq)) {
-							if ((targetDisplayMode == null)
-									|| (current.getBitsPerPixel() > targetDisplayMode
-											.getBitsPerPixel())) {
-								targetDisplayMode = current;
-								freq = targetDisplayMode.getFrequency();
-							}
-						}
-
-						// if we've found a match for bpp and frequence against
-						// the
-						// original display mode then it's probably best to go
-						// for this one
-						// since it's most likely compatible with the monitor
-						if ((current.getBitsPerPixel() == Display
-								.getDesktopDisplayMode().getBitsPerPixel())
-								&& (current.getFrequency() == Display
-										.getDesktopDisplayMode().getFrequency())) {
-							targetDisplayMode = current;
-							break;
-						}
-					}
-				}
-			} else {
-				targetDisplayMode = new DisplayMode(width, height);
-			}
-
-			if (targetDisplayMode == null) {
-				System.out.println("Failed to find value mode: " + width + "x"
-						+ height + " fs=" + fullscreen);
-				return;
-			}
-
-			Display.setDisplayMode(targetDisplayMode);
-			Display.setFullscreen(fullscreen);
-
-		} catch (LWJGLException e) {
-			System.out.println("Unable to setup mode " + width + "x" + height
-					+ " fullscreen=" + fullscreen + e);
-		}
 	}
 
 	public ByteBuffer loadIcon(String filename, int width, int height)
